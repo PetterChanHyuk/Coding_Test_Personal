daf9a1de04f15705d4953ad34c16c3bde1b385b3
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-03-27 15:25:58 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int h, m;
+	int result_h, result_m;
+	cin >> h >> m;
+
+	result_m = m - 45;
+	result_h = h;
+	if (result_m < 0)
+	{
+		result_h--;
+		result_m += 60;
+		if (result_h < 0)
+			result_h += 24;
+	}
+
+	cout << result_h<< " " << result_m << endl;
+
+
+}

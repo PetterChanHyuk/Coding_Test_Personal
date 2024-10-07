185d9c6a39137a980fa98f77873ae1603666132b
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-10-07 17:24:41 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -1,23 +1,12 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	vector<string> a(100);
-	string b;
-	int i = 0;
-	for (i; i < 100; i++)
-	{
-		getline(cin, b);
-		if (b.length() == 0)
-			break;
-		a[i] = b;
-	}
-	for (int j = 0; j < i; j++)
-	{
-		cout << a[j] << endl;
-	}
-	return 0;
+int main() {
+    char line[101];  // 각 줄이 최대 100글자이므로 101로 선언 (널 문자를 포함해야 함)
+    
+    while (cin.getline(line, 101)) {  // 최대 100글자까지 입력받음
+        cout << line << endl;
+    }
+
+    return 0;
 }

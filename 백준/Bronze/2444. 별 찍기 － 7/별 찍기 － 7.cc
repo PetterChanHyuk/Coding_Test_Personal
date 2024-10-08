bb50fcd98d82d94b04ded28ca8ac9df8307557bc
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-10-08 10:42:09 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = N; j > i; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < 2*i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = N - 1; i >= 1; i--) {
+        for (int j = N; j > i; j--)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < 2*i - 1; k++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}

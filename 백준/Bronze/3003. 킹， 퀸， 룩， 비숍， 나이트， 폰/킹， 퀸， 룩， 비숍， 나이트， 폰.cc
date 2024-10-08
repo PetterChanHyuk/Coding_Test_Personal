aa12e0e39163f78d61a3e9bdde46a146ff72a0fa
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-10-08 10:23:36 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int k, q, l, b, kn, p;
+    int x;
+    cin >> k >> q >> l >> b >> kn >> p;
+    x = 1 - k;
+    cout << x << " ";
+    x = 1 - q;
+    cout << x << " ";
+    x = 2 - l;
+    cout << x << " ";
+    x = 2 - b;
+    cout << x << " ";
+    x = 2 - kn;
+    cout << x << " ";
+    x = 8 - p;
+    cout << x << " ";
+    return 0;
+}

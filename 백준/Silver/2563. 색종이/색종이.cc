fae5f8a01297c6bba21fbcbb9ce2ac35483547d7
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-10-12 00:12:13 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -5,21 +5,17 @@ int main()
 {
     int n;
     int x, y;
+    int count = 0;
     cin >> n;
-    int answer[101][101] = {0, };
+    int answer[101][101] = { 0, };
     for (int i = 0; i < n; i++) {
         cin >> x >> y;
-        for (int j = x; j < x+10; j++) {
-            for (int k = y; k < y+10; k++) {
-                answer[j][k] = 1;
-            }
-        }
-    }
-    int count=0;
-    for (int l = 0; l < 101; l++) {
-        for (int m = 0; m < 101; m++) {
-            if (answer[l][m] == 1) {
-                count++;
+        for (int j = x; j < x + 10; j++) {
+            for (int k = y; k < y + 10; k++) {
+                if (!answer[j][k]) {
+                    answer[j][k] = 1;
+                    count++;
+                }
             }
         }
     }

aae2166f0d5e258bd7abaff87f3a9b284f70e25a
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-11-20 21:46:46 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -4,20 +4,19 @@ using namespace std;
 
 int main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
     int N, X;
     cin >> N >> X;
     vector<int> A(N);
-
-    for (int i = 0; i < N; i++)
+    for(int i=0;i<N;i++)
     {
         cin >> A[i];
-        if (X > A[i])
-        {
-            cout << A[i];
-        }
-        else
-            continue;
-        cout << ' ';
     }
-    return 0;
+    for(int j=0;j<N;j++)
+    {
+        if(A[j]<X)
+            cout << A[j] << ' ';
+    }
 }

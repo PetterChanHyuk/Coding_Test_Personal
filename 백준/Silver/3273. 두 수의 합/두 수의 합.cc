3c4b66381de0746da3e174b91850fb3d7e4e14c0
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-11-24 22:19:01 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,22 +1,23 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int a[1000001] = {};
-bool occur[2000001];
+vector<bool> b(1000001);
 int n, x;
-int main()
-{
+int ans = 0;
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	
-	int ans = 0;
 	cin >> n;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	cin >> x;
-
+	vector<int> a(n);
 	for (int i = 0; i < n; i++) {
-		if (x - a[i] > 0 && occur[x - a[i]]) ans++;
-		occur[a[i]] = true;
+		cin >> a[i];
+	}
+	cin >> x;
+	for (int i = 0; i < n; i++) {
+		if (x - a[i] > 0 && b[x - a[i]])
+			ans++;
+		b[a[i]] = true;
 	}
 	cout << ans;
 }

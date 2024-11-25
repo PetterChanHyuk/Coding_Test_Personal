9adbee0206f60381e866eb371251a7621ca2b91a
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2024-11-25 20:14:56 +0900

    [Bronze II] Title: Strfry, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.cc
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -2,12 +2,12 @@
 using namespace std;
 
 string strfry(string s1, string s2) {
-	int a[27] = { 0, };
-	int b[27] = { 0, };
+	int a[26] = { 0, };
+	int b[26] = { 0, };
 	for (char c1 : s1){	a[c1 - 'a']++; }
 	for (char c2 : s2){	b[c2 - 'a']++; }
 	
-	for (int i = 0; i < 27; i++)
+	for (int i = 0; i < 26; i++)
 	{
 		if (a[i] != b[i])
 			return "Impossible";

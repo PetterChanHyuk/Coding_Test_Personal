5c2785f95f5ad5a4c21d051d7f4df648023a4420
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2025-09-26 18:57:58 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2200 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,44 +1,42 @@
 #include<iostream>
 #include<queue>
 #include<utility>
+
+#define X first
+#define Y second
 using namespace std;
 
-int board[102][102];
-bool vis[102][102];
-int cnt[102][102];
-int dx[4] = { 1, 0, -1, 0 };
-int dy[4] = { 0, 1, 0, -1 };
+string board[102];
+int dist[102][102];
 
-int main()
-{
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
+int n, m;
+
+
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, m;
 	cin >> n >> m;
-
 	for (int i = 0; i < n; i++) {
-		string s;
-		cin >> s;
-		for (int j = 0; j < m; j++) {
-			board[i + 1][j + 1] = s[j]-'0';
-		}
+		cin >> board[i];
 	}
+	for (int i = 0; i < n; i++) fill(dist[i], dist[i] + m, -1); //전부 -1로 채우기
+
 	queue<pair<int, int>> q;
-	q.push({ 1, 1 });
-	cnt[1][1] = 1;
-	vis[1][1] = 1;
+	q.push({ 0,0 });
+	dist[0][0] = 0;
 	while (!q.empty()) {
-		pair<int, int> cur = q.front(); q.pop();
+		auto cur = q.front(); q.pop();
 		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-			if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
-			if (vis[nx][ny] || board[nx][ny] != 1) continue;
-			vis[nx][ny] = 1;
-			q.push({ nx, ny });
-			cnt[nx][ny] = cnt[cur.first][cur.second] + 1;
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+			if (nx<0 || nx>=n || ny<0 || ny>=m) continue;
+			if (board[nx][ny] != '1' || dist[nx][ny]>=0) continue;
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+			q.push({ nx,ny });
 		}
 	}
-	cout << cnt[n][m];
+	cout << dist[n - 1][m - 1] + 1;
 }

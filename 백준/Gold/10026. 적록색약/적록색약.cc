12f502a0e8dce326d1cde53e64765328edd27fd8
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2025-11-06 19:02:58 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -1,90 +1,127 @@
 #include<iostream>
 #include<queue>
 #include<utility>
-// #include<algorithm>
 using namespace std;
 
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-int cnt;
-int total1;
-int total2;
 string board[101];
-int vis[101][101];
-char color[3]{ 'R', 'G', 'B' };
-
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
+bool vis[101][101];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int cnt1 = 0;
+int cnt2 = 0;
+int main(){
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
 
+	for (int i = 0; i < n; i++) {
 		cin >> board[i];
 	}
-	queue<pair<int, int>> q;
-	for (int k = 0; k < 3; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (board[i][j] == color[k]&&vis[i][j]!=1) {
-					vis[i][j] = 1;
-					q.push({ i,j });
-					while (!q.empty()) {
-						auto cur = q.front(); q.pop();
-						for (int dir = 0; dir < 4; dir++) {
-							int nx = cur.first + dx[dir];
-							int ny = cur.second + dy[dir];
-							if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-							if (board[nx][ny] != color[k] || vis[nx][ny]) continue;
-							q.push({ nx,ny });
-							vis[nx][ny] = 1;
-						}
-					}
-					cnt++;
-				}
-			}
-		}
-	}
-	total1 = cnt;
-	cnt = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			vis[i][j] = 0;
-		}
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (board[i][j] == 'R') {
-				board[i][j] = 'G';
-			}
-		}
-	}
-	for (int k = 1; k < 3; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (board[i][j] == color[k] && vis[i][j] != 1) {
-					vis[i][j] = 1;
-					q.push({ i,j });
-					while (!q.empty()) {
-						auto cur = q.front(); q.pop();
-						for (int dir = 0; dir < 4; dir++) {
-							int nx = cur.first + dx[dir];
-							int ny = cur.second + dy[dir];
-							if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-							if (board[nx][ny] != color[k] || vis[nx][ny]) continue;
-							q.push({ nx,ny });
-							vis[nx][ny] = 1;
-						}
-					}
-					cnt++;
-				}
-			}
-		}
-	}
-	total2 = cnt;
 
-	cout << total1 << " " << total2 << '\n';
+	queue<pair<int, int>> q;
+	
+	for(int i=0;i<n;i++){
+		for (int j = 0; j < n; j++) {
+			if (vis[i][j] == 1||board[i][j]!='R') continue;
+			q.push({ i,j });
+			vis[i][j] = 1;
+			cnt1++;
+			while (!q.empty()) {
+				auto cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.first + dx[dir];
+					int ny = cur.second + dy[dir];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board[nx][ny] != 'R' || vis[nx][ny]) continue;
+					q.push({ nx,ny });
+					vis[nx][ny] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis[i][j] == 1 || board[i][j] != 'G') continue;
+			q.push({ i,j });
+			vis[i][j] = 1;
+			cnt1++;
+			while (!q.empty()) {
+				auto cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.first + dx[dir];
+					int ny = cur.second + dy[dir];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board[nx][ny] != 'G' || vis[nx][ny]) continue;
+					q.push({ nx,ny });
+					vis[nx][ny] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis[i][j] == 1 || board[i][j] != 'B') continue;
+			q.push({ i,j });
+			vis[i][j] = 1;
+			cnt1++;
+			while (!q.empty()) {
+				auto cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.first + dx[dir];
+					int ny = cur.second + dy[dir];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board[nx][ny] != 'B' || vis[nx][ny]) continue;
+					q.push({ nx,ny });
+					vis[nx][ny] = 1;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		fill(vis[i], vis[i] + n, 0);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis[i][j] == 1) continue;
+			if (!(board[i][j] == 'R' || board[i][j] == 'G')) continue;
+			q.push({ i,j });
+			vis[i][j] = 1;
+			cnt2++;
+			while (!q.empty()) {
+				auto cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.first + dx[dir];
+					int ny = cur.second + dy[dir];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (!(board[nx][ny] == 'R' || board[nx][ny] == 'G') || vis[nx][ny]) continue;
+					q.push({ nx,ny });
+					vis[nx][ny] = 1;
+				}
+			}
+		}
+	}for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (vis[i][j] == 1 || board[i][j] != 'B') continue;
+			q.push({ i,j });
+			vis[i][j] = 1;
+			cnt2++;
+			while (!q.empty()) {
+				auto cur = q.front(); q.pop();
+				for (int dir = 0; dir < 4; dir++) {
+					int nx = cur.first + dx[dir];
+					int ny = cur.second + dy[dir];
+
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					if (board[nx][ny] != 'B' || vis[nx][ny]) continue;
+					q.push({ nx,ny });
+					vis[nx][ny] = 1;
+				}
+			}
+		}
+	}
+	cout << cnt1 << " " << cnt2 << '\n';
 	return 0;
 }

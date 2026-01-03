4993a3ec020e6b73df064c20d4abfa1ad1383a70
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2026-01-03 18:28:34 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 8 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -2,38 +2,27 @@
 #include<stack>
 using namespace std;
 
-int main()
-{
+int main(){
 	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	
-	int count;
+    cin.tie(0);
+    
 	stack<int> s;
-	long long answer = 0;
-	// 빌딩의 개수 입력
-	cin >> count;
+	long long ans = 0;
+    int n;
+	cin >> n;
 
-	for (int i = 0; i < count; i++) {
-		// 빌딩 높이 입력
-		int height;
-		cin >> height;
-
+	for (int i = 0; i < n; i++) {
+		int h;
+		cin >> h;
+		
 		if (s.empty()) {
-			//비어있으면 높이 push하고 continue
-			s.push(height);
+			s.push(h);
 			continue;
 		}
-		// 안 비어있고 스택의 마지막보다 들어온 빌딩의 높이가 크거나 같으면 top을 pop한다.
-		while (!s.empty() && s.top() <= height) s.pop();
-		// 정답에 스택의 사이즈 + 한다
-		answer += s.size();
-		// 빌딩의 높이 넣는다.
-		s.push(height);
+		
+		while (!s.empty() && s.top() <= h) s.pop();
+		ans += s.size();
+		s.push(h);
 	}
-	// 0+1+1+2+0+1
-	
-
-	cout << answer;
-
+	cout << ans;
 }

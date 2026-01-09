164d74ea4ca7d955cfe4c6bac7f1b0b4005b8c54
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2026-01-09 17:29:07 +0900

    [Gold V] Title: 토마토, Time: 88 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,57 +1,64 @@
 #include<iostream>
 #include<queue>
 #include<utility>
+#define X first
+#define Y second
 using namespace std;
 
-int board[1002][1002];
-int vis[1002][1002];
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
-int cnt[1002][1002];
-int check;
+
+int n, m;
+
+int board[1002][1002];
+int dist[1002][1002];
+
 int main() {
+
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n, m;
 	cin >> m >> n;
+	queue<pair<int, int>> q;
+
+	for (int i = 0; i < n; i++) {
+		fill(dist[i], dist[i] + m, -1);
+	}
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> board[i][j];
-		}
-	}
-	queue<pair<int, int>> q;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (board[i][j] == 1) {
-				q.push({ i,j });
-				vis[i][j] = 1;
+			if (board[i][j] == 1) { 
+				q.push({ i,j }); 
+				dist[i][j] = 0;
 			}
 		}
 	}
-
 	while (!q.empty()) {
-		pair<int, int> cur = q.front(); q.pop();
+		auto cur = q.front(); q.pop();
 		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
 			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-			if (vis[nx][ny] || board[nx][ny] < 0) continue;
+			if (board[nx][ny] != 0 || dist[nx][ny] >= 0) continue;
 			q.push({ nx,ny });
-			vis[nx][ny] = 1;
-			cnt[nx][ny] = cnt[cur.first][cur.second] + 1;
+			dist[nx][ny] = dist[cur.X][cur.Y] + 1;
+			board[nx][ny] = 1;
 		}
 	}
-	int ans = 0;
+	int mx = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (board[i][j] >= 0 && vis[i][j] == 0) {
-				cout << -1 << '\n';
+			if (board[i][j] == 0) {
+				cout << -1;
 				return 0;
 			}
-			if (ans < cnt[i][j]) ans = cnt[i][j];
+			if (mx < dist[i][j]) mx = dist[i][j];
+
 		}
 	}
-	cout << ans << '\n';
+	cout << mx;
 	return 0;
+
+
 }

618f97f7c5e356a61dd5064ce40f1f9f5c6075d7
PetterChanHyuk <137492766+PetterChanHyuk@users.noreply.github.com>
2026-02-04 21:07:20 +0900

    [Silver IV] Title: 듣보잡, Time: 24 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,39 +1,35 @@
 #include<iostream>
 #include<vector>
-#include<unordered_set>
+#include<string>
 #include<algorithm>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
+
 	int n, m;
 	cin >> n >> m;
 
-	unordered_set<string> s1;
-
+	vector<string> s1(n);
 	for (int i = 0; i < n; i++) {
-		string s;
-		cin >> s;
-		
-		s1.insert(s);
+		cin >> s1[i];
 	}
+	sort(s1.begin(), s1.end());
 	vector<string> answer;
-	
+
 	for (int i = 0; i < m; i++) {
 		string s;
 		cin >> s;
-		if (s1.find(s) != s1.end()) {
+
+		if (binary_search(s1.begin(), s1.end(), s)) {
 			answer.push_back(s);
 		}
 	}
 
 	sort(answer.begin(), answer.end());
-
 	cout << answer.size() << '\n';
-	for (string str : answer) {
-		cout << str << '\n';
+	for (string s : answer) {
+		cout << s << '\n';
 	}
-	
-	return 0;
 }
